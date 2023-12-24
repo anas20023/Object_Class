e6f01b5ebf491407508a166165eade8bd20b9fa9
anas20023 <yorder07@gmail.com>
2023-12-24 12:55:25 +0600

    Update signup and login functionality

--- classes.js
+++ classes.js
@@ -51,28 +51,49 @@
 // let Sifat=new CSE("Miel Mahmud Sifat",368,99);
 // console.log(Sifat);
 
+let Sgn_div = document.querySelector("#main_div");
+let signup_top = document.querySelector("#div_sgn");
+let sgn_btn_1 = document.querySelector("#btn_1");
+let sgn_btn_2 = document.querySelector("#btn_2");
+let Form_reg = document.querySelector("#form_sbmt");
+let Form_log = document.querySelector("#form_log");
 
-let Sgn_div=document.querySelector("#main_div");
-let signup_top=document.querySelector("#div_sgn");
-let sgn_btn_1=document.querySelector("#btn_1");
-let sgn_btn_2=document.querySelector("#btn_2");
-
-sgn_btn_1.addEventListener("click",()=>
-{
-    signup_top.classList.remove("flex");
-    signup_top.classList.add("hidden");
-    Sgn_div.classList.remove("hidden");
-    Sgn_div.classList.add("flex");
+sgn_btn_1.addEventListener("click", () => {
+  signup_top.classList.remove("flex");
+  signup_top.classList.add("hidden");
+  Sgn_div.classList.remove("hidden");
+  Sgn_div.classList.add("flex");
 });
-sgn_btn_2.addEventListener("click",()=>
-{
-    signup_top.classList.remove("flex");
-    signup_top.classList.add("hidden");
-    Sgn_div.classList.remove("hidden");
-    Sgn_div.classList.add("flex");
+sgn_btn_2.addEventListener("click", () => {
+  signup_top.classList.remove("flex");
+  signup_top.classList.add("hidden");
+  Sgn_div.classList.remove("hidden");
+  Sgn_div.classList.add("flex");
 });
 
 /// Form Submit Event
 
 let usernameSgn;
 let emailSgn;
+
+Form_reg.addEventListener("submit", (e) => {
+  e.preventDefault();
+  usernameSgn = document.getElementById("inline-full-name").value;
+  emailSgn = document.getElementById("inline-email").value;
+  document.getElementById("inline-full-name").value = "";
+  document.getElementById("inline-email").value = "";
+  location.href = "./login.html";
+});
+
+// Login Form
+
+let userLgn;
+let emailLgn;
+
+Form_log.addEventListener("submit", (e) => {
+  e.preventDefault();
+  userLgn = document.getElementById("login-full-name").value;
+  emailLgn = document.getElementById("login-emaill").value;
+  document.getElementById("login-email").value = "";
+  console.log("Yes");
+});
